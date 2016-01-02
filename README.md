364ccd2212f68a504631be4109a548f505527ab9
Himanshu Shekhar <himanshushekharb16@users.noreply.github.com>
2016-01-02 17:36:44 +0530

    First release

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # proxychecker
+
+The version number is mentioned in the main file : proxychecker.sh
+
+CLI tool to check current proxy configuration in a Linux system.
+This helps while debugging/checking current configurations.
+
+Free software licensed under GNU GPL v2.
+
+How to use this tool?
+-----------------------
+Just execute this tool through bash and it will do its job.
+
+
+What does it depend on?
+-------------------------
+Nothing special apart from standard command line utilities as grep.
+
+
+What does it check?
+--------------------
+It checks for proxy configurations of following three places : Terminal, Desktop environment, and apt (aka software center, not really).
+Technically they are : gsettings : org.gnome.system.proxy
+                       /etc/apt/apt.conf
+                       .bashrc/.bash_profiles
---
+++ proxy_check.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# proxy_check v1.1
+# Author : Himanshu Shekhar < https://github.com/himanshushekharb16/ProxyMan >
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# This is a simple tool to check proxy configurations for GNU/Linux distributions of
+# the Debian/Ubuntu familiy. Helps in debugging.
+# The README.md file provides more information.
+
+
+#!/bin/bash
+if [[ -e "$HOME/.bashrc" ]]; then
+	a=$(grep -i proxy $HOME/.bashrc | wc -l)
+	if [[ a -eq 0 ]]; then
+		echo ".bashrc is not using proxy."
+	else
+		echo "For ~/.bashrc..."
+		grep -i proxy $HOME/.bashrc
+	fi
+else
+	echo ".bashrc does not exist."
+fi
+
+echo
+
+if [[ -e "$HOME/.bash_profile" ]]; then
+	a=$(grep -i proxy $HOME/.bash_profile | wc -l)
+	if [[ a -eq 0 ]]; then
+		echo ".bash_profile is not using proxy."
+	else
+		echo "For ~/.bash_profile..."
+		grep -i proxy $HOME/.bash_profile
+	fi
+else
+	echo ".bash_profile does not exist."
+fi
+
+echo
+
+if [[ -e "/etc/apt/apt.conf" ]]; then
+	echo "This is apt.conf ..."
+	cat "/etc/apt/apt.conf"
+else
+	echo "apt is not using proxy."
+fi
+
+echo
+
+mode=$(gsettings get org.gnome.system.proxy mode)
+if [[ $mode == "'none'" ]]; then
+	echo "The desktop environment is not using any proxy settings."
+	echo "Thus, gsettings configurations are ineffective."
+elif [[ $mode == "'manual'" ]]; then
+	echo "The desktop environment is using manual proxy settings."
+	echo "Thus, following gsettings configurations are effective."
+	gsettings list-recursively org.gnome.system.proxy
+else
+	echo "We cannot determine the type of settings. Sorry :("
+fi
